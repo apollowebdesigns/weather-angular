b53acf85554bc541baa3d8a4ea9979f6d182ec4c
Andy Evans <andrewevans0@outlook.com>
2018-08-15 16:38:27 +0100

    Added angular material to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {MatToolbarModule, MatMenuModule, MatButtonModule, MatCheckboxModule, MatIconModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
@@ -10,6 +11,7 @@ import { ReadComponent } from './read/read.component';
 import { CreateComponent } from './create/create.component';
 import { ObservationsComponent } from './observations/observations.component';
 import { LatestdataComponent } from './latestdata/latestdata.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -23,6 +25,12 @@ import { LatestdataComponent } from './latestdata/latestdata.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
     StoreModule.forRoot({
       weather: reducer
     })
