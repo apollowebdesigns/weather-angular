c4f7beea8ca9e96d888552ce064a23d509abaeb4
Andy Evans <andrewevans0@outlook.com>
2018-08-20 19:10:43 +0100

    adding some new files

--- src/app/observations/observations.component.spec.ts
+++ src/app/observations/observations.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { ObservationsComponent } from './observations.component';
+import {Store, StoreModule} from '@ngrx/store';
 
 describe('ObservationsComponent', () => {
   let component: ObservationsComponent;
@@ -8,7 +10,9 @@ describe('ObservationsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ObservationsComponent ]
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      declarations: [ ObservationsComponent ],
+      imports: [StoreModule.provideStore({ demoSlice })]
     })
     .compileComponents();
   }));
